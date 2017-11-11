7538d8df9ae9aa3ec722efb7eae05f99414b35c3
Aditya <aditya.radika@gmail.com>
2017-11-11 07:19:33 +0700

    fibbonaci update

--- soding-fibonacci-test.php
+++ soding-fibonacci-test.php
@@ -1,15 +1,29 @@
+<?php
+	$link = "http://".$_SERVER['HTTP_HOST']."/fibonacci/soding-fibonacci-test.php";
+?>
+<form action="" method="GET">
+	<h1>Enter your number for Fibonacci</h1>
+	<input name="number">
+	<input type="submit" name="submit">
+	<a href="<?php echo $link; ?>"><input type="button" name="test" value="Reset"></a>
+</form>
+
 <?php 
+	$m = 8;
+	if (isset($_GET['submit'])){
+		$m = $_GET['number'];
+	}
 	$a = 0;
 	$b = 1;
-	$m = 32;
+	
 	
 	for($i=1;$i <= $m; $i++){
 		$c = $a + $b; 
 		$a = $b; 
 		$b = $c; 
-		echo $c."<br/>";
+		echo $c.", ";
 		$n = $n + $c;
 	}
-	echo "sum = ". $n . "<br>";
+	echo "<br><br>sum = ". $n . "<br>";
 	echo "even-valued = ". $n / $m;
 ?>
